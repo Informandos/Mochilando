2280605ae910e8f7991282c19f9f7be9bc3e07a3
analiju <jujucarvalho.souza@yahoo.com.br>
2018-07-29 21:28:09 -0300

    Login ok

--- web/js/webvalida.js
+++ web/js/webvalida.js
@@ -65,29 +65,3 @@ function validarCamposDiario() {
 /* 
  Todo que o programa fazer na parte nos html deve passa aqui para depois ir para o controller
  */
-
-/*function validarCamposLogin() {
-    var frm = document.frmLogin;
-    var email = frm.email.value;
-    var senha = frm.senha.value;
-
-    if (email == "") {
-        alert("Favor, preencha o campo email!");
-        frm.email.focus();
-        return false;
-    } else if (senha == "") {
-        alert("Favor, preencha o campo senha!");
-        frm.senha.focus();
-        return false;
-    } else {
-        caminhourl = "/Mochilando/AnalizarLogin?acao=Logar";
-        document.forms[0].action = caminhourl;
-        window.document.forms[0].submit();
-        return true;
-    }
-}*/
-/*
- separe cada fução 
- */
-/*---------------------------------------------------------------------------------------------------*/
-
--- web/paginainicial.jsp
+++ web/paginainicial.jsp
@@ -4,8 +4,10 @@
     Author     : Juliana
 --%>
 
+<%@page import="controller.Logar"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
+
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
