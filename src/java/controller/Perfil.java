24fb20d9d02688cff6465800cc0ac6fc050f0185
analiju <jujucarvalho.souza@yahoo.com.br>
2018-07-30 21:25:37 -0300

    perfil

--- src/java/controller/Perfil.java
+++ src/java/controller/Perfil.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -60,3 +61,38 @@ public class Perfil implements Logica {
     }
 
 }
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import controller.interfacelogica.Logica;
+import javax.servlet.http.HttpServletRequest;
+import model.domain.Usuario;
+
+/**
+ *
+ * @author Juliana
+ */
+public class Perfil implements Logica {
+
+    @Override
+    public String execute(HttpServletRequest request) throws Exception {
+        String paginaJsp = "";
+        Usuario usr = (Usuario) request.getSession().getAttribute("usuario");
+        if (usr == null) {
+            System.out.println("Usuario nao encontrado");
+            String erro = "Usuario nao encontrado!";
+            request.setAttribute("erro", erro);
+            paginaJsp = "/erro.jsp";
+        } else {
+            paginaJsp = "/Perfil.jsp";
+        }
+        return paginaJsp;
+    }
+
+}
+>>>>>>> 945a4a25c12f4e7aa73abe761469f6525e3fc809
